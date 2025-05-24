51322a88636f1cc3ff6ddfba1afca2e271810b03
Shivani Vellanki <shivanivellanki@Mac-3396.lan>
2025-05-24 19:49:45 -0400

    bot.js commented and add the code in index.html exit

--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Analytics object before getting:', window.analytics);
-    let insideData = getAnalytics();
-    console.log('Analytics data retrieved:', insideData);
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.log('Analytics object before getting:', window.analytics);
+//     let insideData = getAnalytics();
+//     console.log('Analytics data retrieved:', insideData);
 
-    // Set customData just before showing bot
-    document.getElementById('chatBtn').onclick = function () {
-        if (window.koreBot && window.koreChatCfg) {
-            window.koreChatCfg.botOptions.botInfo.customData = insideData;
-            window.koreBot.show(window.koreChatCfg);
-        } else {
-            console.error("koreBot or koreChatCfg is not defined");
-        }
-    };
-});
+//     // Set customData just before showing bot
+//     document.getElementById('chatBtn').onclick = function () {
+//         if (window.koreBot && window.koreChatCfg) {
+
+//             window.koreChatCfg.botOptions.botInfo.customData = insideData;
+//             window.koreBot.show(window.koreChatCfg);
+
+//         } else {
+//             console.error("koreBot or koreChatCfg is not defined");
+//         }
+//     };
+// });
